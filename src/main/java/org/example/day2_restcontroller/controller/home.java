7b8fe90231440c6f2cffce3304f2bd677af5bdbb
tushar <tusharbansal3260@gmail.com>
2026-01-19 15:19:42 +0530

    5 aadmi

--- src/main/java/org/example/day2_restcontroller/controller/home.java
+++ src/main/java/org/example/day2_restcontroller/controller/home.java
@@ -4,12 +4,22 @@ import org.example.day2_restcontroller.model.StudentModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.*;
+
 @RestController
 public class home {
     @GetMapping("/")
-    public StudentModel getStudent(){
-        StudentModel student = new StudentModel(1,"Tushar","tusharbansal@gmail.com");
-        return student;
+    public List<StudentModel> getStudents() {
 
+        List<StudentModel> students = new ArrayList<>();
+
+        students.add(new StudentModel(1, "Tushar", "tushar@gmail.com"));
+        students.add(new StudentModel(2, "Rahul", "rahul@gmail.com"));
+        students.add(new StudentModel(3, "Aman", "aman@gmail.com"));
+        students.add(new StudentModel(4, "Neha", "neha@gmail.com"));
+        students.add(new StudentModel(5, "Pooja", "pooja@gmail.com"));
+
+        return students;
     }
+
 }
